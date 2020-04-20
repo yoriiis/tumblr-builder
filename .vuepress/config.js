46400accc27cd8709b3e014d3d822f7c67e9a7c2
yoriiis <yoriiis@users.noreply.github.com>
2020-04-20 17:35:01 +0000

    Deploy to GitHub pages

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,7 +34,7 @@ module.exports = {
 		repo: 'yoriiis/tumblr-builder',
 		repoLabel: 'Github',
 		docsDir: 'docs',
-		docsBranch: 'develop',
+		docsBranch: 'master',
 		editLinks: true,
 		editLinkText: 'Edit this page',
 		smoothScroll: true,
--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "6c1db7cd886ddec7431a68ac9c802719"
+    "revision": "143336f7d036d7b009d709e8dcf8439e"
   },
   {
     "url": "assets/css/0.styles.207a34e1.css",
@@ -38,8 +38,8 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.fe8e8106.js",
-    "revision": "a2eac85dc1c5c7934b4339952fb7ff11"
+    "url": "assets/js/10.54495d2e.js",
+    "revision": "ff48b3a62aa6644e29d35f76c32018ea"
   },
   {
     "url": "assets/js/11.0e613f9c.js",
@@ -86,44 +86,44 @@ self.__precacheManifest = [
     "revision": "b3bed8e826819a2cd427e2dd5c4dce94"
   },
   {
-    "url": "assets/js/8.ff53c860.js",
-    "revision": "d72489ff876f1034765ab6068a52b1cb"
+    "url": "assets/js/8.03686727.js",
+    "revision": "e85adf99e79a1c6bcce01cd8934113cd"
   },
   {
-    "url": "assets/js/9.74da50ff.js",
-    "revision": "3fb6e02a540ffbbfecb269eacc80b5e0"
+    "url": "assets/js/9.f57b192a.js",
+    "revision": "ae1e27731f7de8414d29a6d19e7c733a"
   },
   {
-    "url": "assets/js/app.23987518.js",
-    "revision": "5aa94487d8599a1b1371e19d6efc0a18"
+    "url": "assets/js/app.873d12d5.js",
+    "revision": "6bb1bc6b735a7a20689d79ebbb6db5b2"
   },
   {
     "url": "guide/available-methods.html",
-    "revision": "3509444948402c21d759d4b2962835fb"
+    "revision": "97c3a201d275df1c0621112a19dfbcb5"
   },
   {
     "url": "guide/custom-pages.html",
-    "revision": "b797b2f3ca4318d7a1ea32347c373590"
+    "revision": "7390109dc59a1e355249af5ed3f71f1e"
   },
   {
     "url": "guide/custom-posts.html",
-    "revision": "1ccefee24eb1224d7a014c88b8bb97a2"
+    "revision": "bb1b2ef5557076c8103215cfb64c40a3"
   },
   {
     "url": "guide/deploying.html",
-    "revision": "ac33c165ae55c7679e904f97cb9ffde9"
+    "revision": "4b949f757cb48cf6a0c0a7bf808117b0"
   },
   {
     "url": "guide/getting-started.html",
-    "revision": "b48fb0e105c5acafd4ceff3bd210f0a9"
+    "revision": "84140d01885771256ec4ce90c3d72075"
   },
   {
     "url": "guide/how-it-works.html",
-    "revision": "11ce7951291f489efc78bfccc1ffbd8f"
+    "revision": "c9986d6b87143b48bc6faefd586d0008"
   },
   {
     "url": "guide/index.html",
-    "revision": "3371e349c4eaaeaa8a06fb3aab0d5efa"
+    "revision": "33e969f5ef0d1ae29945d93783b82d78"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -139,7 +139,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "24d77c458d927a8904c3156144c5337d"
+    "revision": "cdd78e4ff616c3c8bbc966ddb41a8c9a"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
